21ce4027c4c1898465d732f0a0122d397669c05b
Gabriel Yasuhiro Zoppello Kanashiro <gyasuhir@c2r4p5.42sp.org.br>
2024-08-19 19:20:43 -0300

    add first ex folder

---
+++ c-00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyasuhir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 14:14:53 by gyasuhir          #+#    #+#             */
+/*   Updated: 2024/08/16 15:06:59 by gyasuhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	<unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letra;
+
+	letra = 'z';
+	while (letra >= 'a')
+	{
+		write(1, &letra, 1);
+		letra--;
+	}
+	return ;
+}
---
+++ shell-00/README.md
@@ -0,0 +1,7 @@
+# Ex00
+
+# Ex01
+## Trocar o tamanho do arquivo
+truncate -s 40 testShell01
+## Trocar creation date
+touch -t 202406012342.00 testShell00
