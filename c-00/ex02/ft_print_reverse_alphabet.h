af3346202858df376ce11c09b31b1c14893ebde5
Gabriel Yasuhiro Zoppello Kanashiro <gyasuhir@c2r4p5.42sp.org.br>
2024-08-19 19:22:16 -0300

    add header and main files

---
+++ c-00/ex02/ft_print_reverse_alphabet.h
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.h                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyasuhir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 11:27:02 by gyasuhir          #+#    #+#             */
+/*   Updated: 2024/08/16 15:01:48 by gyasuhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_PRINT_REVERSE_ALPHABET_H
+# define FT_PRINT_REVERSE_ALPHABET_H
+
+void	ft_print_reverse_alphabet(void);
+#endif
